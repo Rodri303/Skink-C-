1e708e8a158e79d8811a74acbe3f699373a8d87e
Rodri303 <sauraart@gmail.com>
2026-08-30 23:54:14 -0300

    Fase 02 - Corrección BrushControls

--- src/ui/brush/BrushControls.hpp
+++ src/ui/brush/BrushControls.hpp
@@ -1,3 +1,19 @@
 #pragma once
+
 #include <QWidget>
-namespace Skink::Ui::Brush { class BrushControls final : public QWidget { Q_OBJECT public: explicit BrushControls(QWidget* parent=nullptr); signals: void brushSizeChanged(int value); void opacityChanged(int value); void pressureSensitivityChanged(int value); }; }
+
+namespace Skink::Ui::Brush {
+
+class BrushControls final : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit BrushControls(QWidget* parent = nullptr);
+
+signals:
+    void brushSizeChanged(int value);
+    void opacityChanged(int value);
+    void pressureSensitivityChanged(int value);
+};
+
+} // namespace Skink::Ui::Brush
--- src/ui/toolrail/ToolRail.cpp
+++ src/ui/toolrail/ToolRail.cpp
@@ -1,6 +1,30 @@
 #include "ui/toolrail/ToolRail.hpp"
+
 #include <QPushButton>
 #include <QVBoxLayout>
+
 namespace Skink::Ui::ToolRail {
-ToolRail::ToolRail(QWidget* parent):QWidget(parent) { setObjectName("toolStrip"); setFixedSize(42,173); auto* layout=new QVBoxLayout(this); layout->setContentsMargins(4,6,4,6); layout->setSpacing(3); const struct { const char* text; const char* tip; } specs[]={{"B","Pincel"},{"S","Selección"},{"P","Mover"},{"T","Transformar"}}; for(int i=0;i<4;++i){auto* b=new QPushButton(specs[i].text,this);b->setObjectName(i==0?"stripButtonActive":"stripButton");b->setToolTip(specs[i].tip);b->setFixedSize(34,38); layout->addWidget(b); if(i==0)connect(b,&QPushButton::clicked,this,&ToolRail::brushRequested); if(i==1)connect(b,&QPushButton::clicked,this,&ToolRail::selectionRequested); if(i==2)connect(b,&QPushButton::clicked,this,&ToolRail::panRequested); if(i==3)connect(b,&QPushButton::clicked,this,&ToolRail::transformRequested);}}
+
+ToolRail::ToolRail(QWidget* parent)
+    : QWidget(parent)
+{
+    setObjectName("toolStrip");
+    setFixedSize(42, 173);
+    auto* layout = new QVBoxLayout(this);
+    layout->setContentsMargins(4, 6, 4, 6);
+    layout->setSpacing(3);
+
+    struct ToolSpec { const char* label; const char* tooltip; void (ToolRail::*signal)(); };
+    const ToolSpec tools[] = {{"B", "Pincel", &ToolRail::brushRequested}, {"S", "Selección", &ToolRail::selectionRequested}, {"P", "Mover", &ToolRail::panRequested}, {"T", "Transformar", &ToolRail::transformRequested}};
+    for (int index = 0; index < 4; ++index) {
+        const ToolSpec& tool = tools[index];
+        auto* button = new QPushButton(tool.label, this);
+        button->setObjectName(index == 0 ? "stripButtonActive" : "stripButton");
+        button->setToolTip(tool.tooltip);
+        button->setFixedSize(34, 38);
+        layout->addWidget(button);
+        connect(button, &QPushButton::clicked, this, tool.signal);
+    }
 }
+
+} // namespace Skink::Ui::ToolRail
